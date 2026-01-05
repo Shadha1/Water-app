2684457d1a79405aea5cec80f059cf92a819defe
Mikis <wiv868@haw-hamburg.de>
2026-01-05 14:20:44 +0100

    Copie_home

---
+++ app/(tabs)/home.tsx
@@ -0,0 +1,43 @@
+//home.tsx von Shadha
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
+import { useEffect, useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const Home = () => {
+    const [name, setName] = useState("");
+
+    useEffect(() => {
+        const loadData = async () => {
+            const stored = await AsyncStorage.getItem("userInfo");
+            if (stored) {
+                const user = JSON.parse(stored);
+                setName(user.name);
+            }
+        };
+        loadData();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello {name}</Text>
+
+            <Button title="Back" onPress={() => router.replace("/")} />
+        </View>
+    );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        backgroundColor: "#e6f0f2",
+    },
+    title: {
+        fontSize: 24,
+        marginTop: 60,
+        marginBottom: 20,
+    },
+});
